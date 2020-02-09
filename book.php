64784f5c1cbbc39581a842faadff4375e26dfe46
Kristjan21 <kzarka30@gmail.com>
2020-02-09 11:24:52 +0100

    Update

--- book.php
+++ book.php
@@ -1,23 +1,70 @@
 <html>
-    <head><title>Rezervo</title></head>
+    <head><title>Rezervo</title>
+    <style>
+    .log{
+        margin:auto;
+        width:250px;
+        box-shadow:0px 8px 16px 0px rgba(0,0,0,0.9);
+        padding: 80px 40px;
+        margin-top:50px;
+        background: linear-gradient(top, #3c3c3c 0%, #222222 100%);
+        background: -webkit-linear-gradient(top,#3c3c3c 0%,#222222 100%);
+    }
+    h2{
+        margin:0 0 30px 0;
+        padding: 10px;
+        color: #e92c2a;
+        text-align:center;
+    }
+    input{
+        width:100%;
+        margin-bottom: 30px;
+    }
+    input[type=text],input[ type=number]{
+        border: none;
+        outline:none;
+        border: 2px #fff solid;
+        background: transparent;
+        border-radius: 20px;
+        box-sizing: border-box;
+        color:#fff;
+        font-size: 16px;
+        padding: 10px;
+        text-align: center;
+
+    }
+    .kristi{
+        border: none;
+        outline: none;
+        padding: 10px;
+        color: #fff;
+        font-family: Arial;
+        background:#ff267e;
+        cursor: pointer;
+        border-radius: 20px;
+          }
+
+    .kristi :hover{
+        background: black;
+        color: white;
+    }
+    </style>
+    
+    </head>
     <link rel="stylesheet" href="styles/style.css" media="all">
     <body class="noku">
         <form action="" method="POST">
-            <center>
-                <table border="1" width="400" height="300">
-                    <tr>
-                        <td colspan="5" align="center" >Plotesoni formularin per rezervim</td>
-                    </tr>
-                    
-                    <tr><td align="right">Emri juaj:</td><td><input type="text" name="name" required></td></tr>
-                    <tr><td align="right">Numri i personave:</td><td><input type="number" name="number" required></td></tr>
-                    <tr><td align="right">Numri i telefonit:</td><td><input type="number" name="phone" required></td></tr>
-                    <tr>
-                        <td colspan="5" align="center"><input type="submit" name="submit" value="Rezervo"></td>
-                        
-                    </tr>
-                </table>
-            </center>  
+        <div class="log">
+        <h2>Plotesoni formularin e rezervimit</h2>
+        <form>
+        <input  type="text" id="name" placeholder=" Emri juaj" name="name" required>
+        <input  type="number" id="number" placeholder=" Numri i personave" name="number" required>
+        <input  type="number" id="phone" placeholder=" Numri i telefonit" name="phone" required>
+        <input class="kristi" type="submit" name="submit" value="Rezervo">
+
+        </form>
+        </div>
+            
         </form>
     </body>
 </html>
